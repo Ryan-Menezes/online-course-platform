ef4a91d1fb7ebaeba92877b4b47c505ade2172e7
Ryan de Menezes <menezesryan1010@gmail.com>
2023-09-22 15:32:13 -0300

    creating the section container and content card layout

---
+++ resources/views/components/courses/contents/card.blade.php
@@ -0,0 +1,24 @@
+<div class="flex rounded-lg bg-white border border-slate-300 dark:bg-neutral-700 max-h-40">
+    <img class="h-auto w-40 !rounded-none !rounded-l-lg object-cover object-center" src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg" alt="" />
+
+    <div class="flex items-center justify-between p-6 w-full">
+        <div>
+            <h5 class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+                Card title
+            </h5>
+            <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">Text</p>
+            <p class="text-xs text-neutral-500 dark:text-neutral-300">
+                Last updated 3 mins ago
+            </p>
+        </div>
+        <div class="flex flex-col justify-between items-end h-full">
+            <x-dropdown>
+                <x-dropdown.item label="Settings" />
+                <x-dropdown.item label="My Profile" />
+                <x-dropdown.item label="Logout" />
+            </x-dropdown>
+
+            <x-toggle md wire:model.defer="model" />
+        </div>
+    </div>
+</div>
--- resources/views/livewire/courses/edit.blade.php
+++ resources/views/livewire/courses/edit.blade.php
@@ -8,4 +8,15 @@
     <div class="flex justify-end gap-x-4 mt-6">
         <x-button type="submit" form="form-course" primary label="Save" wire:loading.attr="disabled" wire:target="save" />
     </div>
+
+    <div class="block rounded-lg bg-white border mt-8 dark:bg-neutral-700">
+        <h5 class="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium leading-tight dark:border-neutral-600 dark:text-neutral-50">
+            Featured
+        </h5>
+        <div class="p-6 flex flex-col gap-5">
+            <x-courses.contents.card />
+            <x-courses.contents.card />
+            <x-courses.contents.card />
+        </div>
+    </div>
 </div>
